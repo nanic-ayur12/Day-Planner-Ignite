e6a1380b6c06cc49eddd14206853ec47816d05c7
raguladhithyam <mra20031006@gmail.com>
2025-06-28 23:59:54 +0530

    updates

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHpKpknj2cTtQ2vwe84Uws5HI2SgMbYL8",
-  authDomain: "dayplanner-ed69e.firebaseapp.com",
-  projectId: "dayplanner-ed69e",
-  storageBucket: "dayplanner-ed69e.firebasestorage.app",
-  messagingSenderId: "243140453844",
-  appId: "1:243140453844:web:f14c3375976ac5dcdce241"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
